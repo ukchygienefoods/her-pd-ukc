b64af4cdc472a126404ce4439acde3600ceb07dc
ukchygienefoods <ukc.hygienefoods@gmail.com>
2024-06-30 20:10:07 +0200

    changed message template

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.post("/webhook",(req,res)=>{ //i want some
                        messaging_product:"whatsapp",
                        to:from,
                        text:{
-                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                           body:"Hi.. Thank you for your message is "+msg_body
                        }
                    },
                    headers:{
